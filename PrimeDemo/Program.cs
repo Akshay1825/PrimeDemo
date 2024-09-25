8b5ad8924a9dec6285f37b7f727d2ee1e2225deb
Akshay Utekar <akshayutekar1825@gmail.com>
2024-09-25 22:50:11 +0530

    Add project files.

---
+++ PrimeDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using PrimeDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prime prime = new Prime();
+
+            Console.WriteLine("Enter a number :");
+            int number = int.Parse(Console.ReadLine());
+
+            string answer = prime.IsPrime(number)
+                            ? $"{number} is a prime number."
+                            : $"{number} is not a prime number.";
+
+            Console.WriteLine(answer);
+        }
+    }
+}
